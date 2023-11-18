072882b98f602dd0ef98321d1cb81d092e136f21
Stephen <stephensouness@outlook.com>
2023-11-18 12:33:48 +0000

    converted the rendering function to a method within the Todo class

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import "./style.css";
-// PROPERTIES:
-// title
-// description
-// dueDate
-// priority - three levels??
-// notes
-// checlist ?
-
-// projects or separate list of todos
 
 const content = document.querySelector("#content");
+
+const listsContainer = document.createElement("div");
+listsContainer.classList.add("lists-container");
 
 const menuContainer = document.createElement("div");
 const menuHTML = `
@@ -34,6 +28,34 @@ class Todo {
     this.dueDate = dueDate;
     this.priority = priority;
   }
+
+  renderTodoList() {
+    const listContainer = document.createElement("div");
+    listContainer.classList.add("list-container");
+
+    const title = document.createElement("p");
+    title.classList.add("list-title");
+    title.innerText = `Title: ${this.title}`;
+
+    const description = document.createElement("p");
+    description.classList.add("list-description");
+    description.innerText = `Description: ${this.description}`;
+
+    const dueDate = document.createElement("p");
+    dueDate.classList.add("list-due-date");
+    dueDate.innerText = `Due Date: ${this.dueDate}`;
+
+    const priority = document.createElement("p");
+    priority.classList.add("list-priority");
+    priority.innerText = `Priority: ${this.priority}`;
+
+    listContainer.appendChild(title);
+    listContainer.appendChild(description);
+    listContainer.appendChild(dueDate);
+    listContainer.appendChild(priority);
+    listsContainer.appendChild(listContainer);
+    content.appendChild(listsContainer);
+  }
 }
 
 const topic = new Todo(
@@ -57,44 +79,14 @@ const topic3 = new Todo(
   "High"
 );
 
-const listsContainer = document.createElement("div");
-listsContainer.classList.add("lists-container");
+const topic4 = new Todo(
+  "Plan for next year",
+  "Might need to re-align my expectations in this current job market",
+  "22/12/2023",
+  "High"
+);
 
-function addDetailsToDom(obj) {
-  const listContainer = document.createElement("div");
-  listContainer.classList.add("list-container");
-
-  const title = document.createElement("p");
-  title.classList.add("list-title");
-  title.innerText = `Title: ${obj.title}`;
-
-  const description = document.createElement("p");
-  description.classList.add("list-description");
-  description.innerText = `Description: ${obj.description}`;
-
-  const dueDate = document.createElement("p");
-  dueDate.classList.add("list-due-date");
-  dueDate.innerText = `Due Date: ${obj.dueDate}`;
-
-  const priority = document.createElement("p");
-  priority.classList.add("list-priority");
-  priority.innerText = `Priority: ${obj.priority}`;
-
-  listContainer.appendChild(title);
-  listContainer.appendChild(description);
-  listContainer.appendChild(dueDate);
-  listContainer.appendChild(priority);
-  listsContainer.appendChild(listContainer);
-  content.appendChild(listsContainer);
-}
-
-console.log(topic);
-addDetailsToDom(topic);
-
-console.log(topic2);
-addDetailsToDom(topic2);
-
-console.log(topic3);
-addDetailsToDom(topic3);
-
-consoooole.log("jeevy");
+topic.renderTodoList();
+topic2.renderTodoList();
+topic3.renderTodoList();
+topic4.renderTodoList();

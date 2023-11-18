9fa09b436d48879c5769fb2560e3e22073ac2bf6
Stephen <stephensouness@outlook.com>
2023-11-18 11:25:51 +0000

    fixed typo in my function for populating the todo list to the dom

--- src/index.js
+++ src/index.js
@@ -29,26 +29,37 @@ const topic = new Todo(
 
 const topic2 = new Todo(
   "Going for a run",
-  "Need to go for the fecking run of my life",
+  "Need to go for the run of my life",
   "25/11/2023",
   "Level 2"
+);
+
+const topic3 = new Todo(
+  "Connect with people in the job market",
+  "Need to get outside my confort zone and start talking to people since the job boards are too saturated",
+  "22/12/2023",
+  "Level 1"
 );
 
 function addDetailsToDom(obj) {
   const listContainer = document.createElement("div");
   listContainer.classList.add("list-container");
+
   const title = document.createElement("p");
   title.classList.add("list-title");
-  title.innerText = obj.title;
+  title.innerText = `Title: ${obj.title}`;
+
   const description = document.createElement("p");
   description.classList.add("list-description");
-  description.innerText - obj.description;
+  description.innerText = `Description: ${obj.description}`;
+
   const dueDate = document.createElement("p");
   dueDate.classList.add("list-due-date");
-  dueDate.innerText = obj.dueDate;
+  dueDate.innerText = `Due Date: ${obj.dueDate}`;
+
   const priority = document.createElement("p");
   priority.classList.add("list-priority");
-  priority.innerText - obj.priority;
+  priority.innerText = `Priority: ${obj.priority}`;
 
   listContainer.appendChild(title);
   listContainer.appendChild(description);
@@ -62,3 +73,6 @@ addDetailsToDom(topic);
 
 console.log(topic2);
 addDetailsToDom(topic2);
+
+console.log(topic3);
+addDetailsToDom(topic3);

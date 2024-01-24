127ea1587f6e38e1b8f0047312396bb97b262019
Stephen <stephensouness@outlook.com>
2024-01-24 18:19:49 +0000

    added entry points for all pages in webpack config to get app to run in google pages

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,11 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    index: "./src/index.js",
+    todoList: "./src/todoList.js.js",
+    project: "./src/project.js",
+  },
   devtool: "inline-source-map",
   output: {
     filename: "main.js",

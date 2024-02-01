7025d4937162e9fc51e6b2925fa07f1d928f89e0
Stephen <stephensouness@outlook.com>
2024-02-01 18:44:41 +0000

    and again..

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devtool: "inline-source-map",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname),
+    path: path.resolve(__dirname, "dist"),
     publicPath: "",
   },
   module: {
